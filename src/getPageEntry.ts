71787b344d16c8b827d5141be0172e34d8f2ce67
Nuri Yuri <yuri@pokemonworkshop.com>
2026-05-14 11:22:32 +0200

    Implement header and breadcrumb

--- src/getPageEntry.ts
+++ src/getPageEntry.ts
@@ -1,13 +1,13 @@
 import { getEntry, getCollection } from 'astro:content';
 import { sortPages } from './sortPages';
 
-export const getPageEntry = async (slugParam: string, locale: 'en' | 'fr') => {
+export const getPageEntry = async (slugParam: string, locale: 'en' | 'fr', noSubPage?: boolean) => {
   const [slug, tag] = (slugParam ?? 'index').split('_');
   const options = [`${slug}_${locale}`, `${slug}/index_${locale}`];
   const entries = await Promise.all(options.map((o) => getEntry('pages', o)));
   const entry = entries.filter((e) => !!e)[0];
 
-  if (entry.id.endsWith(`/index_${locale}`)) {
+  if (!noSubPage && entry.id.endsWith(`/index_${locale}`)) {
     const subPageSlugPrefix = `${slug}/`;
     const subPages = await getCollection('pages', (e) => e.id.startsWith(subPageSlugPrefix) && e.id.endsWith(locale) && e.id !== entry.id);
     if (!tag) return { entry, subPages: sortPages(subPages).map((e) => e.id), tag };
